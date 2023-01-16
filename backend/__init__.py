07e10c2cbcd892a303c489fbc61ec6083f511c61
Janxa <onizuka-54@live.fr>
2023-01-16 18:10:35 +0100

    Adding a dummy form for uploading folders

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,40 +1,28 @@
 from flask import Flask
-from .settings import aws_config
-import mimetypes
+from backend.upload.upload import upload
 from .extensions import s3
-from .services import file_is_valid
-import os
-from pathlib import Path
 
-app = Flask(__name__)
-print(s3)
-@app.route("/")
-def hello_world():
-    for bucket in s3.buckets.all():
-     print(bucket.name)
-    return ("hello world")
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(upload)
 
-@app.route("/upload")
-def uploading():
-    currentDir=Path(__file__).parent
-    print(currentDir)
-    for root, dirs, files in os.walk("backend/simplestyle_4"):
-        print(files,root,dirs)
-        
-        for name in files:
-            extension=Path(name).suffix
-            contentType=False
-            try: 
-                file_is_valid(extension)
-                contentType=mimetypes.types_map.get(extension.lower())      
-            except: 
-                print(name, "is invalid file type")
+    if app.debug:
+        @app.route("/")
+        def hello_world():
+            
+                return (f""" <html>
+                            <head>
+                            <title></title>
+                            </head>
+                            <body>
+                            <form action="/upload/" method="post" enctype="multipart/form-data"> 
+                            Type Folder Name:<input type="text" name="foldername" /><br/><br/>
+                            Select Folder to Upload: <input type="file" name="files" id="files" multiple directory="" webkitdirectory="" moxdirectory="" /><br/><br/> 
+                            <input type="Submit" value="Upload" name="upload" />
+                            </form>
+                            </body>
+                            </html>""")
 
-            if extension and contentType:
-                print(f' \n uploadging :  {name}  |  {extension} --{contentType} \n')
-                try: 
-                    s3.Bucket('ezdeploy').upload_file(os.path.join(root, name),
-                    os.path.join(root, name),  ExtraArgs={"ContentType": contentType,})
-                    print(f"{'-'*10} {name} uploaded {'-'*10}")
-                except BaseException as e: print(f"error when uploading {name}: \n {str(e)}")
-    return os.path.abspath(__file__)
+
+    return app
+
