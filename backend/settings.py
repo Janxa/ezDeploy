33f0a51cdedfdbfad25bd9b2b40abdfce8cd2492
Janxa <onizuka-54@live.fr>
2023-01-24 08:23:53 +0100

    creating mysqlconfig class

--- backend/settings.py
+++ backend/settings.py
@@ -7,3 +7,8 @@ class aws_config():
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID')
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
     bucket_name=os.getenv('BUCKET_NAME')
+
+class mysql_config():
+    host=os.getenv('MYSQL_HOST')
+    user=os.getenv('MYSQL_USERNAME')
+    password=os.getenv('MYSQL_PASSWORD')
