cdd8c0e59501b92059e4c1f1d1204ff72614cc32
Janxa <onizuka-54@live.fr>
2023-01-24 08:21:44 +0100

    removing pymysql

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from backend.upload.upload import upload
 from .extensions import s3
-
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(upload)
@@ -9,15 +8,15 @@ def create_app():
     if app.debug:
         @app.route("/")
         def hello_world():
-            
+
                 return (f""" <html>
                             <head>
                             <title></title>
                             </head>
                             <body>
-                            <form action="/upload/" method="post" enctype="multipart/form-data"> 
+                            <form action="/upload/" method="post" enctype="multipart/form-data">
                             Type Folder Name:<input type="text" name="foldername" /><br/><br/>
-                            Select Folder to Upload: <input type="file" name="files" id="files" multiple directory="" webkitdirectory="" moxdirectory="" /><br/><br/> 
+                            Select Folder to Upload: <input type="file" name="files" id="files" multiple directory="" webkitdirectory="" moxdirectory="" /><br/><br/>
                             <input type="Submit" value="Upload" name="upload" />
                             </form>
                             </body>
