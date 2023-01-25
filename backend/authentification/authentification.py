be7cff07a2f5bdd34a6e4abc86bb2dc70077ddfc
Janxa <onizuka-54@live.fr>
2023-01-25 19:05:02 +0100

    login now returns a jwt

--- backend/authentification/authentification.py
+++ backend/authentification/authentification.py
@@ -1,25 +1,27 @@
-from flask import Blueprint,request,make_response,jsonify,current_app,g
+from flask import Blueprint,request,make_response,jsonify
 from operator import itemgetter
-from .services import hash_password, CreateUser,login
+from .services import hash_password, CreateUser,login,FindUser
+from flask_jwt_extended import create_access_token
 authentification=Blueprint('authentification',__name__, url_prefix="/authentification")
 
 @authentification.route("/register",methods=['POST'])
 def register_user():
     name,email,password=itemgetter('name','email','password')(request.get_json())
-
-    user = g.db.session.execute(db.select(User).where(User.email==f"{email}")).fetchone()
-    if user == None:
-        hashed_password= hash_password(password)
-        CreateUser(name,email,hashed_password)
-    #insert user in db
-    return make_response(jsonify(f'{email},{password}'))
+    user = FindUser(email)
+    if user:
+        return make_response(jsonify({"error":"Email already used"},409))
+    hashed_password= hash_password(password)
+    CreateUser(name,email,hashed_password)
+    return make_response(jsonify({'Success':'User successfully registered'}),200)
 
 @authentification.route("/login",methods=['POST'])
 def login_user():
     email,password=itemgetter('email','password')(request.get_json())
     try:
-        login(email,password)
+        user=login(email,password)
+        print(user)
+        access_token = create_access_token(identity=user)
     except Exception as e :
             print(e)
-            return make_response(jsonify({"error":"invalid password"}),401)
-    return (password)
+            return make_response(jsonify({"error":str(e)}),401)
+    return  make_response(jsonify({'Success':"User authentified","jwt":access_token}),200)
