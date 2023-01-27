7573ac5dda2c60b74362ee9e421c0e46ba5668d1
Janxa <onizuka-54@live.fr>
2023-01-27 10:06:47 +0100

    changing username: remoiving unique:true

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255),unique=True, nullable=False)
     password=db.Column(db.String(255), nullable=False)
 
