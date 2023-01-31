c21fbe19160c314bd77cdfd2acad898f26676ae0
Janxa <onizuka-54@live.fr>
2023-01-31 11:26:39 +0100

    adding validation token table

--- backend/models.py
+++ backend/models.py
@@ -4,8 +4,28 @@ import sqlalchemy as sa
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255),unique=True, nullable=False)
     password=db.Column(db.String(255), nullable=False)
     validated =db.Column(db.Boolean,unique=False,nullable=False,default=False)
+
+    verification_token = db.relationship('VerificationToken', uselist=False, back_populates='user')
+
+    def __repr__(self):
+        return f"<User(id={self.id},username={self.username}, email={self.email}, verification_token={self.verification_token}, password={self.password}, validated= {self.validated})>"
+
+class VerificationToken(db.Model):
+    __tablename__ = 'verification_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    token = db.Column(db.String(255), unique=True)
+    expiration_date = db.Column(db.DateTime)
+
+    user = db.relationship('User', back_populates='verification_token')
+
+    def __repr__(self):
+        return f"<VerificationToken(user_id={self.user_id}, token={self.token}, expiration_date={self.expiration_date})>"
