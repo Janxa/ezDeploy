f2a076baceefa66ae33f0920bb9e42f49c26e085
Janxa <onizuka-54@live.fr>
2023-01-31 09:16:04 +0100

    Creating send confiramtion email

--- backend/authentification/services.py
+++ backend/authentification/services.py
@@ -1,5 +1,8 @@
 from argon2 import PasswordHasher
 from ..models import db,User
+from flask_mail  import Message
+from backend import mail
+from flask import current_app
 ph = PasswordHasher()
 def hash_password(password):
     hash = ph.hash(password)
@@ -17,7 +20,6 @@ def login(email, password):
     #      db.set_password_hash_for_user(user, ph.hash(password))
 
 
-
 def FindUser(email):
     user = User.query.filter_by(email=email).first()
     return user
@@ -26,3 +28,14 @@ def CreateUser(username,email,password):
     user=User(username=username,email=email,password=password)
     db.session.add(user)
     db.session.commit()
+
+def send__confirmation_email(username,email):
+
+    msg = Message(sender=current_app.config["MAIL_DEFAULT_SENDER"],recipients = [email])
+    msg.subject = "Confirmation email"
+    msg.body = f"""Please Confirm Your Password Using This Link :
+                    Link"""
+    mail.send(msg)
+    # return  make_response("Email Successfully Sent",200)
+    # except Exception as  error:
+    #     return make_response(str(error),400)
