1400b4beb8e583b2e0d1b4d7fe4c40715ea661af
Janxa <onizuka-54@live.fr>
2023-02-09 17:43:05 +0100

    Adding basic footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return ( <footer className="p-4 "><ul className="flex justify-around text-white">
+        <li>placeholder</li>
+        <li>placeholder</li>
+        <li>placeholder</li>
+        <li>placeholder</li>
+    </ul>
+    </footer> );
+}
+
+export default Footer;
