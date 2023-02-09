d330a57540d8a67ee4354db5595f99db76791225
Janxa <onizuka-54@live.fr>
2023-02-09 17:41:02 +0100

    Adding footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   )
 }
