df1efaf8ba46bdc6b67ce74a4b414e1ff38a984d
Janxa <onizuka-54@live.fr>
2023-02-13 11:10:05 +0100

    Creating router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Account from './components/Account'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/Error';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement:<ErrorPage />,
+    children: [
+      {
+        path: "account",
+        element: <Account />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
