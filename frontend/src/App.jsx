ef73324ce902bcf36857689474b3c1b85ff9dc09
Janxa <onizuka-54@live.fr>
2023-02-13 11:09:02 +0100

    Using outlet component to display child routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from './components/Header'
 import Main from './components/Main'
+import Account from './components/Account'
 import './App.css'
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Outlet />
       <Footer />
     </div>
   )
