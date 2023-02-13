968cf01fa8df675ea894b057e7f700280b1a1fe0
Janxa <onizuka-54@live.fr>
2023-02-13 11:09:29 +0100

    Passing display state to account component on link click

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,24 @@
+import { useState } from "react";
+import Menu from "./Menu";
 function Header() {
-    return ( <header className="flex justify-between py-5 px-5 h-16 md:px-16 shadow-md">
-        <p className="text-color-yellow-primary font-bold text-2xl self-center ">EZ hosting</p>
+    const [visible,setVisible] = useState(false)
+    return (
+    <header className="flex justify-between py-4 px-5 h-16 md:px-16 shadow-md sticky overflow-visible">
+        <h1 className="text-color-yellow-primary font-bold text-2xl  ">EZ hosting</h1>
 
-        <button onClick='' className="group w-9 h-full self-center items-end flex flex-col justify-between md:hidden">
+        <button onClick={() => setVisible(!visible,console.log(visible))} className="group w-9 h-full self-center items-end flex flex-col justify-between md:hidden">
             <div className="bg-white w-full h-[13%] group-hover:w-4/5 transition-all"></div>
             <div className="bg-white w-full h-[13%] group-hover:w-2/3 transition-all"></div>
             <div className="bg-white w-full h-[13%] group-hover:w-4/5 transition-all"></div>
         </button>
-
-        <ul className="hidden md:flex w-2/3 justify-between  text-white font-medium">
-            <li>placeholder</li>
+        <Menu visible={visible}/>
+        {/* <ul className="hidden ">
             <li>placeholder</li>
             <div className="flex  w-1/3 justify-around">
               <button className="px-2 mr-2">Login</button>
               <button className="bg-color-blue-primary rounded-full px-3">Register</button>
             </div>
-        </ul>
+        </ul> */}
     </header>
      );
 }
