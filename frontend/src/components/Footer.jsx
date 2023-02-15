641cd058d6e322d11055041868fa1544600b94f7
Janxa <onizuka-54@live.fr>
2023-02-15 18:37:33 +0100

    Footer is now fixed bottom

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer() {
-    return ( <footer className="p-4 "><ul className="flex justify-around text-white">
+    return ( <footer className="p-4 "><ul className="fixed bottom-0 left-0 rigt-0 w-full flex justify-around text-white">
         <li>placeholder</li>
         <li>placeholder</li>
         <li>placeholder</li>
