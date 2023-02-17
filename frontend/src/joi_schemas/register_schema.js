8fa7ee1a5c1a68340eb694d554216f4c5fa35fc0
Janxa <onizuka-54@live.fr>
2023-02-17 03:32:43 +0100

    Creating Schema

---
+++ frontend/src/joi_schemas/register_schema.js
@@ -0,0 +1,17 @@
+
+import Joi from 'joi';
+
+const registerSchema = Joi.object({
+    username:Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({tlds:{allow: false }}).required().messages({
+        "string.email": "Please enter a valid email address",
+        "string.empty": "Please insert an email",
+    }),
+    password: Joi.string().min(8).pattern(new RegExp("^[ a-zA-Z0-9!@#$%^&*()_+\-=\[\\]'\"{};:,.<>\/?]+$")).required().messages({
+        'string.pattern.base': 'Password contain invalid characters',
+        'string.min':'Password must be at least 8 characters long',
+        'string.empty': 'Password is required'
+    }),
+});
+
+export {registerSchema};
