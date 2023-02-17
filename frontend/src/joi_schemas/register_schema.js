9d65f92f37bd236b5a5b14ce076775230d16cedb
Janxa <onizuka-54@live.fr>
2023-02-17 03:35:05 +0100

    Creating Schema

--- frontend/src/joi_schemas/register_schema.js
+++ frontend/src/joi_schemas/register_schema.js
@@ -2,7 +2,12 @@
 import Joi from 'joi';
 
 const registerSchema = Joi.object({
-    username:Joi.string().alphanum().min(3).max(30).required(),
+    username:Joi.string().alphanum().min(3).max(30).required().messages({
+        "string.alphanum": "Username should only countain letters and numbers",
+        "string.empty": "Please insert a username",
+        "string.min":"Username too short",
+        "string.max": "Username too long"
+    }),
     email: Joi.string().email({tlds:{allow: false }}).required().messages({
         "string.email": "Please enter a valid email address",
         "string.empty": "Please insert an email",
