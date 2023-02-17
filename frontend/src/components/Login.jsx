e95154a204145c948b5ff1e30e40e6aeb0001801
Janxa <onizuka-54@live.fr>
2023-02-17 03:39:08 +0100

    Making Login component fully styled and fonctionnal

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { loginSchema } from "../joi_schemas/login_schema";
 import axios from "axios";
-function Login() {
+function Login(props) {
 
     const [data,setData] = useState( { email: "", password: "" });
     const [errors,setErrors]=useState({});
@@ -21,26 +21,35 @@ function Login() {
         event.preventDefault();
         let errors = {};
         try {
-        await schema.validateAsync(
-            {email: data["email"], password: data["password"]}
-            );
-
-        const res = await axios.post("/api/authentification/login",data);
-        // props.add_url_to_list(res.data);
-        // lastUrl = res.data;
-        // props.setLastU                        rl(lastUrl );
-        // setData({ url: "", alias: "", allowMod: false })
+            await schema.validateAsync({email: data["email"], password: data["password"]});
         } catch (err) {
-        console.log(err);
-        err.details.forEach(
-            (error) => (console.log(error),errors[error.context.label] = error.message)
-        );
+            console.log("Validation error:", err);
+            err.details.forEach((error) => (console.log(error), errors[error.context.label] = error.message));
+            setErrors(errors);
+            return;
         }
-        setErrors(errors)
-  };
+        try {
+            const res = await axios.post("/api/authentification/login", data);
+            console.log("Login success:", res);
+        } catch (error) {
+            console.log("Login error:", error);
+            if (error.response.status==500) {
+                errors["request"] = "Server Error. Check your connexion or try again later";
+
+            } else if (error.request) {
+                errors["request"] = error.response.data.error;
+            } else {
+                errors["request"] = error.request.statusText;
+            }
+            setErrors(errors);
+            console.log(errors)
+        }
+    };
+
 
     return (
     <form onSubmit={handleSubmit} className="flex flex-col">
+        <h2 className="font-bold text-2xl text-color-yellow-primary  self-center">Sign in</h2>
         <label htmlFor="email" className="font-medium text-sm py-1">Email:</label>
         <input onChange={handleChange} value={data.email} type="email" name="email" id="email" className={errors["email"] ? 'input-invalid': 'input'} />
         {errors['email']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["email"]}</p>}
@@ -48,6 +57,9 @@ function Login() {
         <input onChange={handleChange} value={data.password} type="password" name="password" id="password" className={errors["password"] ? 'input-invalid': 'input'} />
         {errors['password']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["password"]}</p>}
         <button type="submit" className="my-2 py-2 bg-color-blue-primary hover:bg-color-blue-secondary font-medium text-sm rounded-md border-t">Login</button>
+        {errors['request']  && <p className="font-medium text-sm py-1 text-color-red-primary self-center">{errors["request"]}</p>}
+        <p className="text-sm font-light self-center mt-2 " >No account yet ?</p>
+        <button className=" font-medium  text-color-blue-primary underline  decoration-dashed  " onClick={ ()=> props.formSwitch("register")}>Sign up </button>
     </form>
     );
 }
