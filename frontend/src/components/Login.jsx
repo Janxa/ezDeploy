ecaea2a06261392859bff8dab62a9c6df03dc30f
Janxa <onizuka-54@live.fr>
2023-02-18 17:00:17 +0100

    Better Error handling, created a resend email function in case email hasnt been validated

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import { loginSchema } from "../joi_schemas/login_schema";
+import {Link } from 'react-router-dom';
+import ErrorMessage from "./ErrorMessage";
 import axios from "axios";
+import VerifyAccount from "./VerifyAccount";
 function Login(props) {
 
     const [data,setData] = useState( { email: "", password: "" });
     const [errors,setErrors]=useState({});
     const schema=loginSchema;
+    const [emailSent,setEmailSent]=useState(false);
 
     const handleChange = ({currentTarget:input}) => {
         setData((data) => {
@@ -36,31 +40,66 @@ function Login(props) {
             if (error.response.status==500) {
                 errors["request"] = "Server Error. Check your connexion or try again later";
 
+            } else if (error.response.data['Error']=="Not validated") {
+                errors["request"] = (
+                    <ErrorMessage
+                      message="Your email has not been validated, please check your emails or "
+                      onButtonClick={resendEmail}
+                      buttonContent="Resend one"
+                    />
+                );
+
+                }else if (error.response){
+                errors["request"] = error.response.data.error;
+
+                    } else {
+                        errors["request"] = error.request.statusText;
+                }
+            setErrors(errors);
+            console.log(errors)
+        }
+    };
+    const resendEmail= async () =>{
+        try {
+            const res = await axios.post("/api/authentification/resend", {email: data["email"]});
+            setEmailSent(true)
+        } catch (error) {
+            console.log("Login error:", error);
+            if (error.response.status==500) {
+                errors["request"] = "Server Error. Check your connexion or try again later";
+
             } else if (error.request) {
                 errors["request"] = error.response.data.error;
             } else {
                 errors["request"] = error.request.statusText;
             }
-            setErrors(errors);
-            console.log(errors)
         }
     };
 
 
     return (
-    <form onSubmit={handleSubmit} className="flex flex-col">
-        <h2 className="font-bold text-2xl text-color-yellow-primary  self-center">Sign in</h2>
-        <label htmlFor="email" className="font-medium text-sm py-1">Email:</label>
-        <input onChange={handleChange} value={data.email} type="email" name="email" id="email" className={errors["email"] ? 'input-invalid': 'input'} />
-        {errors['email']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["email"]}</p>}
-        <label htmlFor="password" className="text-sm font-medium py-1">Password:</label>
-        <input onChange={handleChange} value={data.password} type="password" name="password" id="password" className={errors["password"] ? 'input-invalid': 'input'} />
-        {errors['password']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["password"]}</p>}
-        <button type="submit" className="my-2 py-2 bg-color-blue-primary hover:bg-color-blue-secondary font-medium text-sm rounded-md border-t">Login</button>
-        {errors['request']  && <p className="font-medium text-sm py-1 text-color-red-primary self-center">{errors["request"]}</p>}
-        <p className="text-sm font-light self-center mt-2 " >No account yet ?</p>
-        <button className=" font-medium  text-color-blue-primary underline  decoration-dashed  " onClick={ ()=> props.formSwitch("register")}>Sign up </button>
-    </form>
+    <div className="flex flex-col">
+        {!emailSent
+            ?
+            <form onSubmit={handleSubmit} className="flex flex-col">
+
+                <h2 className="font-bold text-2xl text-color-yellow-primary  self-center">Sign in</h2>
+                <label htmlFor="email" className="font-medium text-sm py-1">Email:</label>
+                <input onChange={handleChange} value={data.email} type="email" name="email" id="email" className={errors["email"] ? 'input-invalid': 'input'} />
+                {errors['email']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["email"]}</p>}
+                <label htmlFor="password" className="text-sm font-medium py-1">Password:</label>
+                <input onChange={handleChange} value={data.password} type="password" name="password" id="password" className={errors["password"] ? 'input-invalid': 'input'} />
+                {errors['password']  && <p className="font-medium text-sm py-1 text-color-red-primary">{errors["password"]}</p>}
+                <button type="submit" className="my-2 py-2 bg-color-blue-primary hover:bg-color-blue-secondary font-medium text-sm rounded-md border-t">Login</button>
+                {errors['request']  && <p className="font-medium text-sm py-1 text-color-red-primary self-center">{errors["request"]}</p>}
+                <p className="text-sm font-light self-center mt-2 " >No account yet ?</p>
+                <button className=" font-medium  text-color-blue-primary underline  decoration-dashed  " onClick={ ()=> props.formSwitch("register")}>Sign up </button>
+            </form>
+
+            :
+            <VerifyAccount resendEmail={resendEmail} email={data.email} />
+        }
+    </div>
     );
 }
 
