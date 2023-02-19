046c1933c2514584aed06aae1bc048eaf0067e2c
Janxa <onizuka-54@live.fr>
2023-02-19 11:10:11 +0100

    Creating check csrf function

---
+++ backend/middleware.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask_jwt_extended import verify_jwt_in_request, get_jwt
+
+def check_csrf_token():
+    if 'authentification' in request.path:
+        return
+
+    if request.method in ['POST', 'PUT', 'DELETE']:
+        verify_jwt_in_request()
+        jwt = get_jwt()
+        double_submit_token = get_jwt.get('csrf')
+        csrf_token = request.headers.get('X-CSRF-TOKEN')
+        if not double_submit_token or double_submit_token != csrf_token:
+            return {'message': 'Invalid CSRF token'}, 400
