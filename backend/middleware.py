3d379aca08227703edc42503c9408721db9f6006
Janxa <onizuka-54@live.fr>
2023-02-20 19:37:59 +0100

    Removing print statements

--- backend/middleware.py
+++ backend/middleware.py
@@ -8,7 +8,7 @@ def check_csrf_token():
     if request.method in ['POST', 'PUT', 'DELETE']:
         verify_jwt_in_request()
         jwt = get_jwt()
-        double_submit_token = get_jwt.get('csrf')
+        double_submit_token = jwt.get('csrf')
         csrf_token = request.headers.get('X-CSRF-TOKEN')
         if not double_submit_token or double_submit_token != csrf_token:
             return {'message': 'Invalid CSRF token'}, 400
