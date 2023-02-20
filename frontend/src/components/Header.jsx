f1960dea01dbea1b7fd1ae13e7d82a1662b6b354
Janxa <onizuka-54@live.fr>
2023-02-20 09:43:39 +0100

    Passing setVisible from Header to Menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
     <header className="flex justify-between py-4 px-5 h-16 md:px-16 shadow-md sticky overflow-visible">
         <h1 className="text-color-yellow-primary font-bold text-2xl  ">EZ hosting</h1>
 
-        <button onClick={() => setVisible(!visible,console.log(visible))} className="group w-9 h-full self-center items-end flex flex-col justify-between md:hidden">
+        <button onClick={() => setVisible(!visible)} className="group w-9 h-full self-center items-end flex flex-col justify-between md:hidden">
             <div className="bg-white w-full h-[13%] group-hover:w-4/5 transition-all"></div>
             <div className="bg-white w-full h-[13%] group-hover:w-2/3 transition-all"></div>
             <div className="bg-white w-full h-[13%] group-hover:w-4/5 transition-all"></div>
         </button>
-        <Menu visible={visible}/>
+        <Menu visible={visible} setVisible={setVisible}/>
         {/* <ul className="hidden ">
             <li>placeholder</li>
             <div className="flex  w-1/3 justify-around">
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-function Menu({visible}) {
+function Menu({visible,setVisible}) {
     return (
     <div className={visible
         ? 'flex flex-col items-center absolute top-16 left-0 w-full h-screen z-50 md:flex md:w-2/3 md:justify-between text-white md:font-medium md:static md:h-full bg-color-bg-dark pt-4'
         : 'hidden md:flex md:w-2/3 md:justify-between  md:text-white md:font-medium '}>
         <ul className=" flex flex-col md:items-center md:flex-row-reverse md:justify-between w-3/4 md:w-full">
             <li className="flex mb-4 md:mb-0  basis-1/4 w-1/2 md:w-auto justify-around self-center">
-                <Link className="p-2 mx-1 rounded-md bg-color-blue-primary"
+                <Link onClick={() => setVisible(!visible)} className="p-2 mx-1 rounded-md bg-color-blue-primary"
                 to="/account" state={{disp:'login'}}
                 >
                     Login
                 </Link>
-                <Link className="p-2 mx-1 rounded-md bg-color-blue-primary"
+                <Link onClick={() => setVisible(!visible)} className="p-2 mx-1 rounded-md bg-color-blue-primary"
                 to="/account" state={{disp:'register'}}
                 >
                     Register
