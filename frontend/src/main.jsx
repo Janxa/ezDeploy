ae68c4f95fc6962bff01d182f07eedbb8b8f58e2
Janxa <onizuka-54@live.fr>
2023-02-22 20:16:58 +0100

    Adding authprovider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Account from './components/Account'
+import Account from './components/Account/Account'
 import './index.css'
+import { CookiesProvider } from 'react-cookie';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './components/Error';
 import VerifyEmail from './components/VerifyEmail'
+import Dashboard from './components/Dashboard/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 
 const router = createBrowserRouter([
@@ -31,12 +34,18 @@ const router = createBrowserRouter([
           },
         ]
       },
+      {
+        path:"dashboard/*",
+        element:
+        <ProtectedRoute element={<Dashboard />} />
+      }
     ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-
-  </React.StrictMode>,
+    <CookiesProvider>
+      <RouterProvider router={router} />
+    </CookiesProvider>
+  </React.StrictMode>
 )
