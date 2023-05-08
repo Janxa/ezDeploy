81dafe4fc4383d69cd1e0c892285571f609c9595
Janxa <onizuka-54@live.fr>
2023-05-08 17:28:34 +0200

    Restructuring auth service as an object

--- frontend/src/services/authentification.service.js
+++ frontend/src/services/authentification.service.js
@@ -1,26 +1,33 @@
 import axios from "axios";
 const API_URL = "/api/authentification";
 
-class AuthService {
+const AuthService = {
+	async login(email, password) {
+		const res = await axios.post(API_URL + "/login", {
+			email: email,
+			password: password,
+		});
+		localStorage.setItem("user", JSON.stringify(res.data.username));
+		return res;
+	},
+	logout() {
+		localStorage.removeItem("user");
+	},
+	register(username, email, password) {
+		return axios.post(API_URL + "/register", {
+			username: username,
+			email: email,
+			password: password,
+		});
+	},
+	getCurrentUser() {
+		try {
+			const user = JSON.parse(localStorage.getItem("user"));
+			return user;
+		} catch (e) {
+			return e;
+		}
+	},
+};
 
-   async login(email, password) {
-        const res=await axios.post(API_URL+"/login", {email:email, password:password});
-        localStorage.setItem("user", JSON.stringify(res.data.username));
-        return res
-    }
-z
-    logout() {
-        localStorage.removeItem("user");
-    }
-
-    register(username, email, password) {
-
-    return  axios.post(API_URL+"/register", {username:username, email:email, password:password});
-    }
-
-    getCurrentUser() {
-        return JSON.parse(localStorage.getItem('user'));;
-    }
-}
-
-export default new AuthService();
+export default AuthService;
