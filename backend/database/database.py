07cb0b26245ff19959746bd0366717ccd8fc41e1
Janxa <onizuka-54@live.fr>
2023-05-08 17:09:23 +0200

    AddingCreateWebsite, UpdateWebsitestatus,Updatewebsitetask,GetAllWebsites,DeleteWebsiteById,FindWebsiteByTask,FindWebsiteById and Update Website link

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,18 +1,26 @@
-from ..models import db,User,VerificationToken
+from ..models import db,Users,VerificationTokens,Websites
 import secrets
 import hashlib
 from datetime import datetime,timedelta
 import os
+from ..errors import WebsiteNotFoundError
 import base64
 
 
 
-def FindUser(email):
-    user = User.query.filter_by(email=email).first()
-    return user
+def FindUser(email=False,user_id=False):
+    if email:
+        user = Users.query.filter_by(email=email).first()
+        return user
+    if user_id:
+        print("Finding :",user_id)
+        user = Users.query.filter_by(id=user_id).first()
+        print("user found :",user)
+        return user
+
 
 def UserHasToken(user):
-    token = VerificationToken.query.filter_by(user=user).first()
+    token = VerificationTokens.query.filter_by(user=user).first()
     print(token)
     return token
 
@@ -27,7 +35,7 @@ def DeleteToken(token):
 def ValidateToken(token):
     try:
         hashed_token = hashlib.sha256(token.encode()).hexdigest()
-        token = VerificationToken.query.filter_by(token=hashed_token).first()
+        token = VerificationTokens.query.filter_by(token=hashed_token).first()
         user=token.user
         if user.validated == False:
             user.validated=True
@@ -41,11 +49,11 @@ def ValidateToken(token):
 def CreateUser(username,email,password):
     while True:
         user_id=generate_id()
-        user= User.query.filter_by(id=user_id).first()
+        user= Users.query.filter_by(id=user_id).first()
         if not user:
             break
     try:
-        user=User(username=username,email=email,password=password,id=user_id)
+        user=Users(username=username,email=email,password=password,id=user_id)
         db.session.add(user)
         db.session.commit()
 
@@ -69,8 +77,73 @@ def GenerateVerificationToken(user):
     token = secrets.token_hex(16)
     hashed_token = hashlib.sha256(token.encode()).hexdigest()
     expiration_date = datetime.now() + timedelta(hours=24)
-    verification_token = VerificationToken(user_id=user.id, token=hashed_token,expiration_date=expiration_date)
+    verification_token = VerificationTokens(user_id=user.id, token=hashed_token,expiration_date=expiration_date)
     db.session.add(verification_token)
     db.session.commit()
     print("hashed token",hashed_token)
     return token
+
+def CreateWebsite(user_id,name,task_id):
+    print(user_id)
+    user=FindUser(user_id=user_id)
+    if not user:
+        raise Exception(({"message": "User not found", "code": 404}))
+    website_id=generate_id()
+    website=Websites(id=website_id,user_id=user_id, name=name,task=task_id)
+    db.session.add(website)
+    db.session.commit()
+    print("Website added : ",website)
+    return website
+
+def UpdateWebsiteStatus(website,status):
+    website.status=status
+    db.session.commit()
+    return website
+
+def UpdateWebsiteTask(website,task):
+    try:
+        print("website.task",website.task)
+
+        website.task=task
+        print('website.task',website.task)
+        db.session.commit()
+    except Exception as e:
+        raise e
+    return website
+
+def GetAllWebsites(user_id):
+    websites=Websites.query.filter_by(user_id=user_id).all()
+    websites_list = [website.__dict__ for website in websites]
+    for website_dict in websites_list:
+        del website_dict['_sa_instance_state']
+    return websites_list
+
+def DeleteWebsiteById(website_id):
+    website = Websites.query.get(website_id)
+    if website is None:
+        raise WebsiteNotFoundError(f"Website with ID {id} not found.")
+    print("deleting",website.name)
+    db.session.delete(website)
+    print("deleted !")
+
+    db.session.commit()
+def FindWebsiteByTask(task_id):
+    return Websites.query.filter_by(task=task_id).first()
+
+def FindWebsiteById(id):
+    website = Websites.query.filter_by(id=id).first()
+    if website is None:
+        raise WebsiteNotFoundError(f"Website with ID {id} not found.")
+    print("website found", website.name )
+    return website
+
+def UpdateWebsiteLink(website,link):
+    try:
+        print("website.task",website.link)
+
+        website.link=link
+        print('website.link',website.link)
+        db.session.commit()
+    except Exception as e:
+        raise e
+    return website
