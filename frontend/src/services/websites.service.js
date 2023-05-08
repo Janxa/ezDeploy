a9dcb0d16a4e14cb5f25b7b6e5bdeb480189a8a8
Janxa <onizuka-54@live.fr>
2023-05-08 17:30:15 +0200

    Creating getWedsiteById function, making deleteWebsite functional

--- frontend/src/services/websites.service.js
+++ frontend/src/services/websites.service.js
@@ -10,10 +10,14 @@ function getCookie(csrf_access_token) {
 	const parts = value.split(`; ${csrf_access_token}=`);
 	if (parts.length === 2) return parts.pop().split(";").shift();
 }
-const uploadWebsite = async (file) => {
+const getWebsiteById = (website_id) => {
+	return axios.get(API_URL + "getById/" + website_id);
+};
+
+const uploadWebsite = async (file, websiteName) => {
 	const formData = new FormData();
 	formData.append("file", file);
-	formData.append("website_name", "website");
+	formData.append("website_name", websiteName);
 	return await axios.post(API_URL + "upload", formData, {
 		credentials: "same-origin",
 		withCredentials: true,
@@ -24,13 +28,22 @@ const uploadWebsite = async (file) => {
 	});
 };
 
-const deleteWebsite = () => {
-	return axios.delete(API_URL + "delete");
+const deleteWebsite = async (website_id) => {
+	return axios.delete(API_URL + "delete", {
+		data: { website_id: website_id },
+		credentials: "same-origin",
+		withCredentials: true,
+		headers: {
+			"Content-Type": "application/json",
+			"X-CSRF-TOKEN": getCookie("csrf_access_token"),
+		},
+	});
 };
 
 const WebsiteService = {
 	deleteWebsite,
 	uploadWebsite,
 	getWebsites,
+	getWebsiteById,
 };
 export default WebsiteService;
