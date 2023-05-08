54defd6aab0ee3ca23c1e9384eb9ea7dfdbf7ca1
Janxa <onizuka-54@live.fr>
2023-05-08 17:10:58 +0200

    Adding new config

--- backend/extensions/aws_s3.py
+++ backend/extensions/aws_s3.py
@@ -1,5 +1,5 @@
 import boto3
-from ..settings import aws_config
+from ..config import Config
 s3 = boto3.resource('s3',
-aws_access_key_id=aws_config.aws_access_key_id,
-aws_secret_access_key=aws_config.aws_secret_access_key)
+aws_access_key_id=Config.aws_access_key_id,
+aws_secret_access_key=Config.aws_secret_access_key)
