ddea49a82efa4bb384978adbb780a5a986e84184
Janxa <onizuka-54@live.fr>
2023-05-10 16:30:36 +0200

    Adding celery_init_app

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,16 +6,26 @@ from .config import config
 from celery import Celery
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from .database.create_db import create_db
-celery = Celery(__name__,backend=config.RESULT_BACKEND ,broker=config.REDIS_URL)
-db = SQLAlchemy()
+from .extensions import db
+from celery import Celery, Task
 
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config, namespace='CELERY')
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
-    celery.conf.update(app.config)
+    celery_init_app(app)
     migrate = Migrate(app, db)
 
     jwt = JWTManager(app)
