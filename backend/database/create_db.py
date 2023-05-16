dfaa85cac77eebc92ae6452c679f510935669262
Janxa <onizuka-54@live.fr>
2023-05-16 19:47:21 +0200

    Added a function that deletes the db

--- backend/database/create_db.py
+++ backend/database/create_db.py
@@ -14,3 +14,22 @@ def create_db():
         if e.errno==1007:
             print('database already created')
         else:print(e)
+
+def drop_db():
+    try:
+        mydb = mysql.connector.connect(
+            host=config.host,
+            user=config.user,
+            password=config.password
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute(f"DROP DATABASE {config.db_name}")
+        mydb.commit()
+    except mysql.connector.Error as e:
+        if e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist.")
+        else:
+            print(e)
+    finally:
+        mycursor.close()
+        mydb.close()
