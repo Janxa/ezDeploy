5da05de4fa8e7b8f4cc7be497750e7eb7e642ea6
Janxa <onizuka-54@live.fr>
2023-05-19 19:14:38 +0200

    Reworking color scheme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,30 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./index.html",
-            "./src/**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors:{
-      'orange':'#FF7700',
-      'green-valid':'#386641',
-      'green-alternative':'#53A548',
-      'red-invalid':'#A30000',
-      'blue-pale':'#D9DCD6',
-      'blue':'#16425B',
-      'lightblue':'#81C3D7',
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				"green-valid": "#386641",
+				"green-alternative": "#53A548",
+				"red-invalid": "#A30000",
+				"blue-pale": "#D9DCD6",
 
-      'color-bg-dark': '#353535',
-      'color-bg-dark-2':'#292929',
-      'color-yellow-primary':'#F9CB40',
-      'color-blue-primary':'#5688C7',
-      'color-blue-secondary':'#465F7E',
-      'color-green-primary':'#499F68',
-      'color-red-primary':'#DA2C38'
-    }},
-  },
-  plugins: [],
-}
+				chili: {
+					100: "#FFE7E3",
+					200: "#F2BDB3",
+					300: "#E79688",
+					400: "#d67b6b",
+					500: "#D6462E",
+					600: "#AE3825",
+					700: "#852A1B",
+					800: "#5C1C11",
+					900: "#330E07",
+				},
+
+				"color-bg-dark": "#28262B",
+				"color-bg-dark-2": "#18171A",
+				// "color-red-primary": "#D6462E",
+				// "color-green-primary": "#5C946E",
+				// "color-blue-primary": "#4472CA",
+				// "color-blue-secondary": "#9DCBBA",
+				// "color-red-primary": "#FB3640",
+			},
+		},
+	},
+	plugins: [],
+};
