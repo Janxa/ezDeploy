030fd3b0a72fb2296a3ad86764193e8dc1b8f878
Janxa <onizuka-54@live.fr>
2023-05-22 14:47:01 +0200

    Removing useless app.css import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
-import Header from './components/Header'
-import './App.css'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
-import AuthContextProvider from './context/AuthProvider';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import AuthContextProvider from "./context/AuthProvider";
 function App() {
-
-  return (
-    <AuthContextProvider>
-      <div className="App h-screen">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    </AuthContextProvider>
-  )
+	return (
+		<AuthContextProvider>
+			<div className="App h-screen">
+				<Header />
+				<Outlet />
+				<Footer />
+			</div>
+		</AuthContextProvider>
+	);
 }
 
-export default App
+export default App;
