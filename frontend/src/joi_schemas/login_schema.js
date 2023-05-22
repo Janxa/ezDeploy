10abdcdc2098e1e3d257ce7f68f7aef35f7a74d7
Janxa <onizuka-54@live.fr>
2023-05-22 17:49:11 +0200

    Removing minimal size for password

--- frontend/src/joi_schemas/login_schema.js
+++ frontend/src/joi_schemas/login_schema.js
@@ -1,17 +1,20 @@
+import Joi from "joi";
 
-import Joi from 'joi';
+const loginSchema = Joi.object({
+	email: Joi.string()
+		.email({ tlds: { allow: false } })
+		.required()
+		.messages({
+			"string.email": "Please enter a valid email address",
+			"string.empty": "Please insert an email",
+		}),
+	password: Joi.string()
+		.pattern(new RegExp("^[ a-zA-Z0-9!@#$%^&*()_+-=[\\]'\"{};:,.<>/?]+$"))
+		.required()
+		.messages({
+			"string.pattern.base": "Password contain invalid characters",
+			"string.empty": "Password is required",
+		}),
+});
 
-  const loginSchema = Joi.object({
-    email: Joi.string().email({tlds:{allow: false }}).required().messages({
-    "string.email": "Please enter a valid email address",
-    "string.empty": "Please insert an email",
-    }),
-    password: Joi.string().min(8)
-    .pattern(new RegExp("^[ a-zA-Z0-9!@#$%^&*()_+\-=\[\\]'\"{};:,.<>\/?]+$")).required().messages({
-    'string.pattern.base': 'Password contain invalid characters',
-    'string.min':'Password must be at least 8 characters long',
-    'string.empty': 'Password is required'
-    }),
-  });
-
-  export {loginSchema};
+export { loginSchema };
