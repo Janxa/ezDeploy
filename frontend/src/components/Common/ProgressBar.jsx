f97d6768ea15bfdda3ac7abc9c9f997f82267f7f
Janxa <onizuka-54@live.fr>
2023-05-24 20:39:48 +0200

    Adding new color palett

--- frontend/src/components/Common/ProgressBar.jsx
+++ frontend/src/components/Common/ProgressBar.jsx
@@ -4,12 +4,12 @@ const ProgressBar = ({ value, max }) => {
 	const progress = Math.round((value / max) * 100);
 
 	return (
-		<div className="h-4 w-full rounded-full bg-gray-300 relative">
+		<div className="h-4 w-full rounded-full bg-flat-300 relative">
 			<div
-				className="h-full rounded-full bg-color-blue-primary "
+				className="h-full rounded-full bg-chili-500 "
 				style={{ width: `${progress}%`, transition: "width 0.5s ease-in-out" }}
 			/>
-			<span className="text-xs font-bold text-color-bg-dark-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+			<span className="text-xs font-bold text-chili-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 				{`${progress}%`}
 			</span>
 		</div>
