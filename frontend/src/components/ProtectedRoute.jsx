7cf03293cd2b167696423378c369a7d5da5465b5
Janxa <onizuka-54@live.fr>
2023-05-25 20:31:25 +0200

    Modifying navigate according to news route tree

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,12 @@
-import React, { useContext } from 'react';
-import { Route, Routes,Navigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthProvider';
-function PrivateRoute({ element, ...rest }) {
-    const { isLoggedIn } = useContext(AuthContext);
-    console.log(element)
-    console.log("authentified," , isLoggedIn)
-    console.log(isLoggedIn ? "true":"false")
-    return (<>
-        {isLoggedIn ? element  : <Navigate to="/account" replace={true}/>}
-        </>
-    );
-  }
+import React, { useContext } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 
-  export default PrivateRoute;
+function PrivateRoute({ element, ...rest }) {
+	const { isLoggedIn } = useContext(AuthContext);
+	return (
+		<>{isLoggedIn ? element : <Navigate to="/app/account" replace={true} />}</>
+	);
+}
+
+export default PrivateRoute;
