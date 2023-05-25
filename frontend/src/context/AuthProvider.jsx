09d1cd0b5c579bda3751f4a43c9323674ad7b704
Janxa <onizuka-54@live.fr>
2023-05-25 20:34:11 +0200

    Removing console.log

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,38 +1,38 @@
-import React, { createContext, useState,useEffect} from 'react';
-import AuthService from '../services/authentification.service';
+import React, { createContext, useState, useEffect } from "react";
+import AuthService from "../services/authentification.service";
 
 export const AuthContext = createContext();
 
-const AuthContextProvider = ({children}) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    console.log("tryng to get")
-    const currentUser = AuthService.getCurrentUser();
-    console.log(currentUser)
-    if (currentUser) {
-      setIsLoggedIn(true);
-    }
-    setIsLoading(false);
-  }, []);
+const AuthContextProvider = ({ children }) => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		console.log("tryng to get");
+		const currentUser = AuthService.getCurrentUser();
+		console.log(currentUser);
+		if (currentUser) {
+			setIsLoggedIn(true);
+		}
+		setIsLoading(false);
+	}, []);
 
-  const login = () => {
-    console.log("login called")
-    setIsLoggedIn(true);
-    console.log("login state",isLoggedIn)
-  };
+	const login = () => {
+		console.log("login called");
+		setIsLoggedIn(true);
+	};
 
-  const logout = () => {
-    setIsLoggedIn(false);
-  };
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+	const logout = () => {
+		setIsLoggedIn(false);
+	};
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export default AuthContextProvider;
