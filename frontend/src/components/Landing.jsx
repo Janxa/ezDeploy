b7538e78a0c1b39e344764efa9564ae2932bc68e
Janxa <onizuka-54@live.fr>
2023-05-29 19:47:53 +0200

    Created final desktop layout

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -10,37 +10,45 @@ import Button from "./Common/Button";
 
 function Landing() {
 	return (
-		<div className="p-4 h-screen grid grid-cols-2 grid-rows-6 gap-y-20">
-			<h1 className="text-chili-500 text-2xl font-bold col-span-2 ">
+		<div className="p-4 h-screen grid grid-cols-2 grid-rows-6 ">
+			<h1 className="text-chili-500 text-2xl font-bold col-span-1 ">
 				Ez Deploy
 			</h1>
-			<div className=" col-span-2  row-span-3  grid row-start-2 grid-cols-3">
-				<h2 className="text-chili-100 text-7xl items-center flex justify-center text-center flex-col  font-bold">
-					The app that <b className="text-chili-500">deploys</b> your website in{" "}
-					<b className="text-jade-500">3 clicks</b>
+			<div className=" col-span-1  row-start-2 row-end-5  grid grid-rows-6 grid-cols-12">
+				<h2 className="text-chili-100 text-8xl  col-span-12 col-start-2   font-bold">
+					The app that <b className="text-chili-500">deploys</b> your website in
+					<b className="text-jade-500 block "> 3 clicks</b>
 				</h2>
-				<div className="col-span-2 grid grid-cols-3 ">
-					<div className=" col-start-2 flex flex-col items-center ">
-						<p>Build your website</p>
-						<div className="bg-chili-500 rounded-full h-20 w-20  p-4 flex items-center cursor-pointer justify-center">
-							<FontAwesomeIcon icon={faComputer} className="fa-2xl" />
-						</div>
+			</div>
+			<div className="grid col-span-1 row-[span_6_/_span_6] row-start-1  grid-cols-1 grid-rows-7    ">
+				<div className=" flex flex-col items-center row-start-2 row-span-2 scale-110">
+					<p>Build your website</p>
+					<div className="bg-chili-500 rounded-full h-20 w-20  p-4 flex items-center cursor-pointer justify-center">
+						<FontAwesomeIcon icon={faComputer} className="fa-2xl" />
 					</div>
-					<div className=" col-start-2 flex flex-col items-center ">
-						<p>Upload It</p>
-						<div className="bg-chili-500 rounded-full h-20 w-20 p-4 flex items-center  cursor-pointer justify-center">
-							<FontAwesomeIcon icon={faCloudArrowUp} className="fa-2xl " />
-						</div>
+				</div>
+
+				<div className=" flex flex-col items-center row-span-2 scale-110 ">
+					<p>Upload It</p>
+					<div className="bg-chili-500 rounded-full h-20 w-20 p-4 flex items-center  cursor-pointer justify-center">
+						<FontAwesomeIcon icon={faCloudArrowUp} className="fa-2xl " />
 					</div>
-					<div className=" col-start-2 flex flex-col items-center ">
-						<p>Enjoy</p>
-						<div className="bg-chili-500 rounded-full h-20 w-20 p-4 flex items-center  cursor-pointer justify-center">
-							<FontAwesomeIcon icon={faFaceSmileWink} className="fa-2xl " />
-						</div>
+				</div>
+
+				<div className=" flex flex-col items-center  row-span-1 scale-110">
+					<p>Enjoy</p>
+					<div className="bg-chili-500 rounded-full h-20 w-20 p-4 flex items-center  cursor-pointer justify-center">
+						<FontAwesomeIcon icon={faFaceSmileWink} className="fa-2xl " />
 					</div>
 				</div>
 			</div>
-			<Button title="GetStarted" extraStyle="row-start-5 h-full text-3xl" />
+
+			<div className=" grid col-span-1 row-start-6 grid-rows-6  grid-cols-6 ">
+				<Button
+					title="Get Started"
+					extraStyle=" col-span-2 row-span-3 col-start-3  rounded-lg font-medium text-3xl "
+				/>
+			</div>
 		</div>
 	);
 }
