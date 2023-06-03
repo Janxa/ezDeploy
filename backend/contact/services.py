853a9c8eee787a34668ad7b9607748e73cc6030c
Janxa <onizuka-54@live.fr>
2023-06-03 17:56:54 +0200

    Adding contact system using flask mail

---
+++ backend/contact/services.py
@@ -0,0 +1,16 @@
+from flask_mail import Mail, Message
+from flask import current_app, make_response
+
+def send_email(mail_content,object,mail_sender):
+    email_object = f"New e-mail from {app_config.DOMAIN_NAME}'s contact form !"
+    try:
+        if len(mail_content) > 10000:
+           raise Exception("Mail too long")
+
+        msg = Message(email_object,sender =current_app.config["MAIL_DEFAULT_SENDER"],recipients = [current_app.config["MAIL_DEFAULT_SENDER"]])
+        msg.body = f"""message sent by {mail_sender}
+                        {mail_content}"""
+        mail.send(msg)
+        return  make_response("Email Successfully Sent",200)
+    except Exception as  error:
+        return make_response(str(error),400)
