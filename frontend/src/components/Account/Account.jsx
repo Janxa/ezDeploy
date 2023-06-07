89d93a2465e6f89762ac31a3bf87fb0c7ee1e3db
Janxa <onizuka-54@live.fr>
2023-06-07 19:03:29 +0200

    Adding a section to display redirection error messages

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -8,9 +8,10 @@ function Account() {
 	window.history.replaceState({}, document.title);
 	let { state } = useLocation();
 	const [disp, setDisp] = useState(state);
-
+	const [loggedOut, setLoggedOut] = useState(false);
 	useEffect(() => {
 		setDisp(state?.disp || "login");
+		setLoggedOut(state?.error || false);
 	}, [state]);
 
 	const formSwitch = (new_state) => {
@@ -18,7 +19,12 @@ function Account() {
 	};
 
 	return (
-		<main className="mx-auto mt-32 md:mt-32 w-full h-96 lg:w-3/4 flex">
+		<main className="mx-auto mt-32 md:mt-32 w-full h-96 lg:w-3/4 flex flex-col">
+			{loggedOut ? (
+				<p className="mx-auto text-lg text-center mb-12">{loggedOut}</p>
+			) : (
+				false
+			)}
 			<div className="bg-flat-700 w-3/4 md:w-1/2 lg:w-2/3 m-auto shadow-md rounded-md  flex flex-col">
 				{disp === "login" ? <Login formSwitch={formSwitch} /> : false}
 				{disp === "register" ? <Register formSwitch={formSwitch} /> : false}
