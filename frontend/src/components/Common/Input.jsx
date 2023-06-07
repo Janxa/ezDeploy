f112c8e5ee9c401a1e1cf98caa2a4bd8c99accf8
Janxa <onizuka-54@live.fr>
2023-06-07 09:09:54 +0200

    Handling hte case where the pinput is a textarea

--- frontend/src/components/Common/Input.jsx
+++ frontend/src/components/Common/Input.jsx
@@ -17,46 +17,65 @@ function Input({
 	errors = false,
 	valid = true,
 	disabled = false,
+	rows = 8,
 }) {
 	const inputStyle = `
-	${extraStyle}
-	h-full
-	w-full
-	p-1
-	pl-8
-	rounded-sm
-	focus:outline-none
-	focus:bg-jade-200
-	z-10
-	text-flat-800
-	font-medium
+    ${extraStyle}
+    h-full
+    w-full
+    p-1
+    pl-8
+    rounded-sm
+    focus:outline-none
+    focus:bg-jade-200
+    z-10
+    text-flat-800
+    font-medium
     ${!valid && "outline bg-invalid-200 outline-1 outline-invalid-500"}  `;
 
 	let icon = null;
+	let inputElement = null;
 
 	if (name === "password") {
 		icon = <FontAwesomeIcon icon={faKey} className="fa-md" />;
 	} else if (name === "email") {
 		icon = <FontAwesomeIcon icon={faEnvelope} className="fa-md" />;
-	} else if (name == "username") {
+	} else if (name === "username") {
 		icon = <FontAwesomeIcon icon={faUser} className="fa-md" />;
-	} else if (name == "website") {
+	} else if (name === "website") {
 		icon = <FontAwesomeIcon icon={faPen} className="fa-md" />;
 	}
 
+	if (type === "textarea") {
+		inputElement = (
+			<textarea
+				className={inputStyle}
+				onChange={onChange}
+				value={value}
+				name={name}
+				id={id}
+				rows={rows}
+			/>
+		);
+	} else {
+		inputElement = (
+			<input
+				className={inputStyle}
+				onChange={onChange}
+				value={value}
+				type={type}
+				name={name}
+				id={id}
+			/>
+		);
+	}
+
 	return (
-		<div className=" h-full w-full flex flex-col mb-2">
-			<div className="relative  w-full h-4/5">
-				<input
-					className={inputStyle}
-					onChange={onChange}
-					value={value}
-					type={type}
-					name={name}
-					id={id}
-				/>
+		<div className="h-full w-full flex flex-col mb-2">
+			<div className="relative w-full h-4/5">
+				{inputElement}
 				{icon && (
-					<div className="absolute  inset-y-0 flex  items-center pl-2  text-flat-800">
+					<div className="absolute inset-y-0 flex items-center pl-2 text-flat-800">
 						{icon}
 					</div>
 				)}
