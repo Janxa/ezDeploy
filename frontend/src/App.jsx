4e1cb7a9328978dad6bb549ee780ae3843f1fc60
Janxa <onizuka-54@live.fr>
2023-06-09 17:57:20 +0200

    Adding AuthVerify component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import AuthContextProvider from "./context/AuthProvider";
+import AuthVerify from "./components/Common/AuthVerify";
+import AuthService from "./services/authentification.service";
 function App() {
 	return (
 		<AuthContextProvider>
@@ -10,6 +12,7 @@ function App() {
 				<Outlet />
 				<Footer />
 			</div>
+			<AuthVerify logOut={AuthService.logout} />
 		</AuthContextProvider>
 	);
 }
