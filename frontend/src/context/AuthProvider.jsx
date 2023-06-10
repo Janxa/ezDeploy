b9f748533e9850d432a48ccb7406598f8bf31909
Janxa <onizuka-54@live.fr>
2023-06-10 10:38:45 +0200

    Renaling authcontextprovider to auth provider, replacing isLoggedIn with user, handling the loggin and logout inside the auth provider, using the auth service

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,38 +1,39 @@
 import React, { createContext, useState, useEffect } from "react";
 import AuthService from "../services/authentification.service";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
-const AuthContextProvider = ({ children }) => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
-		console.log("tryng to get");
 		const currentUser = AuthService.getCurrentUser();
 		console.log(currentUser);
 		if (currentUser) {
-			setIsLoggedIn(true);
+			setUser(currentUser);
 		}
 		setIsLoading(false);
 	}, []);
 
-	const login = () => {
-		console.log("login called");
-		setIsLoggedIn(true);
+	const login = async (email, password) => {
+		const username = (await AuthService.login(email, password)).data.username;
+		console.log("username == ", username);
+		setUser(username);
 	};
 
-	const logout = () => {
-		setIsLoggedIn(false);
+	const logout = async () => {
+		AuthService.logout();
+		setUser(false);
 	};
 	if (isLoading) {
 		return <div>Loading...</div>;
 	}
 
 	return (
-		<AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+		<AuthContext.Provider value={{ user, login, logout }}>
 			{children}
 		</AuthContext.Provider>
 	);
 };
 
-export default AuthContextProvider;
+export { AuthContext, AuthProvider };
