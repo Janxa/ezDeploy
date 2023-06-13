1e50796dd8ba7a03b32a00389835b5f0217273b2
Janxa <onizuka-54@live.fr>
2023-06-13 15:21:34 +0200

    Adding parameters, adding a call to delete_from_s3 task for cleanup

--- backend/tasks/revoke.py
+++ backend/tasks/revoke.py
@@ -2,11 +2,11 @@ from celery.result import AsyncResult
 from celery.exceptions import Terminated
 from ..database.database import DeleteWebsiteById
 from celery import shared_task
-
+from .delete import delete_from_s3
 @shared_task()
-def revoke_task(task_id,website_id):
+def revoke_task(task_id,website_id,website_name,user_id):
 # Revoke a task by its task ID
     result = AsyncResult(task_id)
     print("---------------------------------revoking-------------------------",result)
     result.revoke(terminate=True)
-    DeleteWebsiteById(website_id)
+    delete_from_s3.delay( user_id, website_name, website_id, delete_from_db=True)
