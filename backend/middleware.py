c394d9731b9a19ef7ceef54df87ad23c89f4cd02
Janxa <onizuka-54@live.fr>
2023-06-13 15:04:58 +0200

    Changing error code ti 401

--- backend/middleware.py
+++ backend/middleware.py
@@ -10,10 +10,10 @@ def check_csrf_token():
 
         try:verify_jwt_in_request()
         except Exception as e:
-            return make_response({'message':str(e)},400)
+            return make_response({'message':str(e)},401)
         jwt = get_jwt()
         print(jwt)
         double_submit_token = jwt.get('csrf')
         csrf_token = request.headers.get('X-CSRF-TOKEN')
         if not double_submit_token or double_submit_token != csrf_token:
-            return make_response({'message': 'Invalid CSRF token'}, 400)
+            return make_response({'message': 'Invalid CSRF token'}, 401)
