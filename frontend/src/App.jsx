a1628bcfee5ac073b1d381441ffe0c3f52f944de
Janxa <onizuka-54@live.fr>
2023-06-14 16:07:54 +0200

    Removing footer, removing Authverify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
-import AuthVerify from "./components/Common/AuthVerify";
-import AuthService from "./services/authentification.service";
 function App() {
 	return (
 		<AuthProvider>
 			<div className="App h-screen">
 				<Header />
 				<Outlet />
-				<Footer />
 			</div>
-			<AuthVerify logOut={AuthService.logout} />
 		</AuthProvider>
 	);
 }
