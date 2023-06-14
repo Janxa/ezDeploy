b78ad2de1a65eb0aa5997a42212e50cdd0c3c44e
Janxa <onizuka-54@live.fr>
2023-06-14 09:29:52 +0200

    Reworking contact form for better readability

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import { useEffect, useState } from "react";
 import { contact_schema } from "../joi_schemas/contact_schema";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../../styles/react-toastify-custom.css";
-import Input from "./Common/Input";
+import Input from "./Common/Input/Input";
+import Textarea from "./Common/Input/Textarea";
 import Button from "./Common/Button";
 import emailjs from "@emailjs/browser";
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function Contact() {
-	const [mail_sender, setmail_sender] = useState("");
-	const [mail_content, setmail_content] = useState("");
-	const [mail_sender_name, setMail_sender_name] = useState("");
+	const [data, setData] = useState({
+		mail_sender: "",
+		mail_sender_name: "",
+		mail_content: "",
+	});
 	const [errors, setErrors] = useState({});
 	const schema = contact_schema;
 
@@ -21,49 +24,69 @@ function Contact() {
 		import.meta.env.VITE_MAIL_KEY,
 	];
 
-	const sendEmail = async (event) => {
-		event.preventDefault();
-		let new_errors = {};
-		let templateParams = {
-			mail_sender: mail_sender,
-			mail_sender_name: mail_sender_name,
-			mail_content: mail_content,
-		};
-		try {
-			await schema.validateAsync(
-				{
-					mail_sender: mail_sender,
-					mail_content: mail_content,
-					mail_sender_name: mail_sender_name,
-				},
-				{ abortEarly: false }
-			);
-			const res = await toast.promise(
-				emailjs.send(mail_service, mail_template, templateParams, mail_key),
-				{
-					pending: "Loading",
-					success: "📧 Your email has been sent !",
-					error: "🤯 Error, could not send email ",
-				},
-				{ theme: "dark" }
-			);
-			console.log(res);
-			setmail_sender("");
-			setmail_content("");
-			setMail_sender_name("");
-		} catch (err) {
-			console.log(err);
-			err.details.forEach(
-				(error) => (new_errors[error.context.label] = error.message)
-			);
-			setErrors(new_errors);
+	const handleChange = ({ currentTarget: input }) => {
+		if (errors[input.name]) {
+			setErrors((errors) => {
+				const updatedErrors = { ...errors };
+				delete updatedErrors[input.name];
+				return updatedErrors;
+			});
 		}
+		setData((data) => {
+			return {
+				...data,
+				[input.name]: input.value,
+			};
+		});
 	};
 
-	// might implement real time error handling
-	useEffect(() => {
-		console.log("error", errors);
-	});
+	const verifyFields = (templateParams) => {
+		const result = schema.validate(templateParams, { abortEarly: false });
+		let new_errors = {};
+		result.error?.details.forEach(
+			(error) => (new_errors[error.context.label] = error.message)
+		);
+		setErrors(new_errors);
+		return new_errors;
+	};
+	const sendEmail = async (templateParams) => {
+		const res = await toast.promise(
+			emailjs.send(mail_service, mail_template, templateParams, mail_key),
+			{
+				pending: "Loading",
+				success: "📧 Your email has been sent!",
+				error: "🤯 Error, could not send email",
+			},
+			{ theme: "dark" }
+		);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		let templateParams = {
+			mail_sender: data["mail_sender"],
+			mail_sender_name: data["mail_sender_name"],
+			mail_content: data["mail_content"],
+		};
+
+		let new_errors = verifyFields(templateParams);
+		if (Object.keys(new_errors).length === 0) {
+			try {
+				sendEmail(templateParams);
+				setData({ mail_sender: "", mail_sender_name: "", mail_content: "" });
+			} catch (err) {
+				let new_errors = {};
+				if (err.details) {
+					err.details.forEach(
+						(error) => (new_errors[error.context.label] = error.message)
+					);
+				} else {
+					new_errors.general = "An error occurred while sending the email.";
+				}
+				setErrors(new_errors);
+			}
+		}
+	};
 	return (
 		<main className="mt-28 md:mt-32 w-11/12 m-auto flex  flex-col">
 			<ToastContainer
@@ -76,57 +99,49 @@ function Contact() {
 				pauseOnFocusLoss
 				draggable
 				pauseOnHover
-				theme="light"
 			/>
 
 			<h2 className="bg-flat-700  w-4/5 md:w-1/2 lg:w-11/12 mx-auto text-2xl p-2 font-bold text-center  shadow-md rounded-md m-2">
 				Contact
 			</h2>
-			<form
-				onSubmit={sendEmail}
-				className=" [&>*]:p-1  bg-flat-700 flex flex-col h-full  w-4/5  md:w-1/2 lg:w-11/12  overflow-hidden  mx-auto p-2 shadow-md rounded-md"
-			>
+			<form className=" [&>*]:p-1  bg-flat-700 flex flex-col h-full  w-4/5  md:w-1/2 lg:w-11/12  overflow-hidden  mx-auto p-2 shadow-md rounded-md">
 				<p>
 					Any question ? Send me an e-mail through this form and i'll answer you
 					asap !
 				</p>
-				<label for="mail_sender" title="So I can answer your mail !">
+				<label for="mail_sender_name" title="So I can answer your mail !">
 					Your name :
 				</label>
 				<Input
-					className={errors["mail_sender_name"] ? "input-invalid" : "input"}
 					type="text"
-					name="username"
+					icon={faUser}
+					name="mail_sender_name"
 					id="mail_sender_name"
-					value={mail_sender_name}
-					onChange={(e) => setMail_sender_name(e.target.value)}
+					value={data["mail_sender_name"]}
+					onChange={handleChange}
 					errors={errors["mail_sender_name"]}
 				/>
 				<label for="mail_sender" title="So I can answer your mail !">
 					Your email adress :
 				</label>
 				<Input
-					className={errors["mail_sender"] ? "input-invalid" : "input"}
-					type="text"
-					name="email"
+					type="email"
+					icon={faEnvelope}
+					name="mail_sender"
 					id="mail_sender"
-					value={mail_sender}
-					onChange={(e) => setmail_sender(e.target.value)}
+					value={data["mail_sender"]}
+					onChange={handleChange}
 					errors={errors["mail_sender"]}
 				/>
 
 				<label for="contact_message" title="Cant exceed 10 000 characters">
 					Your Message :
 				</label>
-				<Input
-					// className="h-full w-full p-1 pl-8 rounded-sm focus:outline-none focus:bg-jade-200 z-10 text-flat-800 font-medium"
-					name="contact_message"
+				<Textarea
+					name="mail_content"
 					id="contact_message"
-					type="textarea"
-					cols="30"
-					rows="10"
-					value={mail_content}
-					onChange={(e) => setmail_content(e.target.value)}
+					value={data["mail_content"]}
+					onChange={handleChange}
 					errors={errors["mail_content"]}
 				/>
 
@@ -134,7 +149,7 @@ function Contact() {
 					extraStyle="font-medium text-sm my-2 py-2 rounded-xl w-1/2 m-auto"
 					type="submit"
 					title="Send Email !"
-					onClick={sendEmail}
+					onClick={handleSubmit}
 				/>
 			</form>
 		</main>
