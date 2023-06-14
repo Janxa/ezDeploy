a9b36572fe564c43073ce1361d4d8464f4cb567d
Janxa <onizuka-54@live.fr>
2023-06-14 15:37:19 +0200

    moving pages in pages folder

---
+++ frontend/src/components/Pages/Account/Register/Register.jsx
@@ -0,0 +1,164 @@
+import { useState } from "react";
+import { registerSchema } from "../../../../joi_schemas/register_schema";
+import { faKey, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import AuthService from "../../../../services/authentification.service";
+import Input from "../../../Common/Input/Input.jsx";
+import Button from "../../../Common/Button";
+import EmailVerificationMessage from "../../../EmailVerificationMessage";
+import axios from "axios";
+function Register(props) {
+	const [data, setData] = useState({ username: "", email: "", password: "" });
+	const [errors, setErrors] = useState({});
+	const [emailsent, setEmailsent] = useState(false);
+	const [emailresent, setEmailResent] = useState(false);
+	const schema = registerSchema;
+
+	const handleChange = ({ currentTarget: input }) => {
+		if (errors[input.name]) {
+			setErrors((errors) => {
+				const updatedErrors = { ...errors };
+				delete updatedErrors[input.name];
+				return updatedErrors;
+			});
+		}
+		setData((data) => {
+			console.log(data, [input.name], input.value);
+			return {
+				...data,
+				[input.name]: input.value,
+			};
+		});
+	};
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		let errors = {};
+		const validationResult = schema.validate(data, { abortEarly: false });
+		if (validationResult.error) {
+			// Validation failed
+			console.error(validationResult.error.details);
+			const error_list = { ...errors };
+
+			for (let error of validationResult.error.details) {
+				error_list[error.context.label] = error.message;
+			}
+
+			setErrors(error_list);
+			return;
+		}
+		try {
+			const res = await axios.post("/api/authentification/register", data);
+			setEmailsent(true);
+		} catch (error) {
+			console.log("Login error:", error);
+			if (error.response.status == 500) {
+				errors["request"] =
+					"Server Error. Check your connexion or try again later";
+			} else if (error.request) {
+				errors["request"] = error.response.data.error;
+			} else {
+				errors["request"] = error.request.statusText;
+			}
+			setErrors(errors);
+			console.log(errors);
+		}
+	};
+	const resendEmail = async () => {
+		try {
+			const res = await axios.post("/api/authentification/resend", {
+				email: data["email"],
+			});
+			setEmailResent(true);
+		} catch (error) {
+			console.log("Login error:", error);
+			if (error.response.status == 500) {
+				errors["request"] =
+					"Server Error. Check your connexion or try again later";
+			} else if (error.request) {
+				errors["request"] = error.response.data.error;
+			} else {
+				errors["request"] = error.request.statusText;
+			}
+		}
+	};
+	return (
+		<div className=" flex flex-col ">
+			{!emailsent ? (
+				<form onSubmit={handleSubmit} className="flex flex-col p-5 ">
+					<h2 className="font-bold text-2xl text-color-yellow-primary  self-center ">
+						Register
+					</h2>
+					<label for="username" className="font-medium text-sm py-1">
+						Username:
+					</label>
+					<Input
+						icon={faUser}
+						onChange={handleChange}
+						value={data.username}
+						type="text"
+						id="username_input"
+						valid={errors["username"] ? false : true}
+						errors={errors["username"]}
+					/>
+
+					<label for="email" className="text-sm font-medium py-1">
+						Email:
+					</label>
+
+					<Input
+						icon={faEnvelope}
+						onChange={handleChange}
+						value={data.email}
+						name="email"
+						id="email_input"
+						valid={errors["email"] ? false : true}
+						errors={errors["email"]}
+					/>
+
+					<label for="password" className="text-sm font-medium py-1">
+						Password:
+					</label>
+
+					<Input
+						onChange={handleChange}
+						value={data.password}
+						name="password"
+						icon={faKey}
+						id="password_input"
+						valid={errors["password"] ? false : true}
+						errors={errors["password"]}
+					/>
+
+					{errors["request"] && (
+						<p className="text-md my-2 text-center w-full  text-invalid-500 self-center">
+							{errors["request"]}
+						</p>
+					)}
+
+					<Button
+						title="Register"
+						type="submit"
+						disabled={
+							data.email && data.password && data.username ? false : true
+						}
+					/>
+					<p className="text-sm font-light self-center mt-2 ">
+						Already got an account ?
+					</p>
+					<button
+						className="font-medium   underline  decoration-dashed  "
+						onClick={() => props.formSwitch("login")}
+					>
+						Sign in
+					</button>
+				</form>
+			) : (
+				<EmailVerificationMessage
+					resendEmail={resendEmail}
+					email={data.email}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default Register;
---
+++ frontend/src/components/Pages/Contact/Contact.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { useEffect, useState } from "react";
+import { contact_schema } from "../../../joi_schemas/contact_schema";
+import "react-toastify/dist/ReactToastify.css";
+import "../../styles/react-toastify-custom.css";
+import Input from "../../Common/Input/Input";
+import Textarea from "../../Common/Input/Textarea";
+import Button from "../../Common/Button";
+import emailjs from "@emailjs/browser";
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+function Contact() {
+	const [data, setData] = useState({
+		mail_sender: "",
+		mail_sender_name: "",
+		mail_content: "",
+	});
+	const [errors, setErrors] = useState({});
+	const schema = contact_schema;
+
+	const [mail_service, mail_template, mail_key] = [
+		import.meta.env.VITE_MAIL_SERVICE,
+		import.meta.env.VITE_MAIL_TEMPLATE,
+		import.meta.env.VITE_MAIL_KEY,
+	];
+
+	const handleChange = ({ currentTarget: input }) => {
+		if (errors[input.name]) {
+			setErrors((errors) => {
+				const updatedErrors = { ...errors };
+				delete updatedErrors[input.name];
+				return updatedErrors;
+			});
+		}
+		setData((data) => {
+			return {
+				...data,
+				[input.name]: input.value,
+			};
+		});
+	};
+
+	const verifyFields = (templateParams) => {
+		const result = schema.validate(templateParams, { abortEarly: false });
+		let new_errors = {};
+		result.error?.details.forEach(
+			(error) => (new_errors[error.context.label] = error.message)
+		);
+		setErrors(new_errors);
+		return new_errors;
+	};
+	const sendEmail = async (templateParams) => {
+		const res = await toast.promise(
+			emailjs.send(mail_service, mail_template, templateParams, mail_key),
+			{
+				pending: "Loading",
+				success: "📧 Your email has been sent!",
+				error: "🤯 Error, could not send email",
+			},
+			{ theme: "dark" }
+		);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		let templateParams = {
+			mail_sender: data["mail_sender"],
+			mail_sender_name: data["mail_sender_name"],
+			mail_content: data["mail_content"],
+		};
+
+		let new_errors = verifyFields(templateParams);
+		if (Object.keys(new_errors).length === 0) {
+			try {
+				sendEmail(templateParams);
+				setData({ mail_sender: "", mail_sender_name: "", mail_content: "" });
+			} catch (err) {
+				let new_errors = {};
+				if (err.details) {
+					err.details.forEach(
+						(error) => (new_errors[error.context.label] = error.message)
+					);
+				} else {
+					new_errors.general = "An error occurred while sending the email.";
+				}
+				setErrors(new_errors);
+			}
+		}
+	};
+	return (
+		<main className="mt-28 md:mt-32 w-11/12 m-auto flex  flex-col">
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+
+			<h2 className="bg-flat-700  w-4/5 md:w-1/2 lg:w-11/12 mx-auto text-2xl p-2 font-bold text-center  shadow-md rounded-md m-2">
+				Contact
+			</h2>
+			<form className=" [&>*]:p-1  bg-flat-700 flex flex-col h-full  w-4/5  md:w-1/2 lg:w-11/12  overflow-hidden  mx-auto p-2 shadow-md rounded-md">
+				<p>
+					Any question ? Send me an e-mail through this form and i'll answer you
+					asap !
+				</p>
+				<label for="mail_sender_name" title="So I can answer your mail !">
+					Your name :
+				</label>
+				<Input
+					type="text"
+					icon={faUser}
+					name="mail_sender_name"
+					id="mail_sender_name"
+					value={data["mail_sender_name"]}
+					onChange={handleChange}
+					errors={errors["mail_sender_name"]}
+				/>
+				<label for="mail_sender" title="So I can answer your mail !">
+					Your email adress :
+				</label>
+				<Input
+					type="email"
+					icon={faEnvelope}
+					name="mail_sender"
+					id="mail_sender"
+					value={data["mail_sender"]}
+					onChange={handleChange}
+					errors={errors["mail_sender"]}
+				/>
+
+				<label for="contact_message" title="Cant exceed 10 000 characters">
+					Your Message :
+				</label>
+				<Textarea
+					name="mail_content"
+					id="contact_message"
+					value={data["mail_content"]}
+					onChange={handleChange}
+					errors={errors["mail_content"]}
+				/>
+
+				<Button
+					extraStyle="font-medium text-sm my-2 py-2 rounded-xl w-1/2 m-auto"
+					type="submit"
+					title="Send Email !"
+					onClick={handleSubmit}
+				/>
+			</form>
+		</main>
+	);
+}
+
+export default Contact;
