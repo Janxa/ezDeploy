99d1d61ac9bc85addc2e69157a5c4dd395f7a0e4
Janxa <onizuka-54@live.fr>
2023-06-15 17:56:32 +0200

    Adding new useAuth hook

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,7 +1,11 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import AuthService from "../services/authentification.service";
 import LoadingWheel from "../components/Common/LoadingWheel";
 const AuthContext = createContext();
+
+const useAuth = () => {
+	return useContext(AuthContext);
+};
 
 const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(false);
@@ -17,6 +21,7 @@ const AuthProvider = ({ children }) => {
 	}, []);
 
 	const login = async (email, password) => {
+		console.log("login");
 		const username = (await AuthService.login(email, password)).data.username;
 		setUser(username);
 	};
@@ -40,4 +45,4 @@ const AuthProvider = ({ children }) => {
 	);
 };
 
-export { AuthContext, AuthProvider };
+export { AuthContext, AuthProvider, useAuth };
