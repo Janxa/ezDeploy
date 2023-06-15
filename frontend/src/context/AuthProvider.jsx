70f766488b86736210fadff58fbbf7b8cad005f9
Janxa <onizuka-54@live.fr>
2023-06-15 19:41:27 +0200

    Setting state using Auth service directly, removing now useless isLoading and useEffect

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import AuthService from "../services/authentification.service";
-import LoadingWheel from "../components/Common/LoadingWheel";
 const AuthContext = createContext();
 
 const useAuth = () => {
@@ -8,20 +7,9 @@ const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
-	const [user, setUser] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		const currentUser = AuthService.getCurrentUser();
-		if (currentUser) {
-			setUser(currentUser);
-		}
-
-		setIsLoading(false);
-	}, []);
+	const [user, setUser] = useState(() => AuthService.getCurrentUser());
 
 	const login = async (email, password) => {
-		console.log("login");
 		const username = (await AuthService.login(email, password)).data.username;
 		setUser(username);
 	};
@@ -30,13 +18,6 @@ const AuthProvider = ({ children }) => {
 		AuthService.logout();
 		setUser(false);
 	};
-	if (isLoading) {
-		return (
-			<div className="flex flex-col w-full h-screen justify-center items-center">
-				<LoadingWheel />
-			</div>
-		);
-	}
 
 	return (
 		<AuthContext.Provider value={{ user, login, logout }}>
