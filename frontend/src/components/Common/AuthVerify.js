00aca6325d9219e07ad531103b7ec2df8d93b366
Janxa <onizuka-54@live.fr>
2023-06-15 17:58:46 +0200

    Removing getCookieValue, using new cookie service instead

--- frontend/src/components/Common/AuthVerify.js
+++ frontend/src/components/Common/AuthVerify.js
@@ -1,21 +1,14 @@
+import cookieService from "../../services/cookie.service";
+
 //This function verifies if the user is still logged
 //It is intended to be used over protected routes to ensure that the user accessing
 //the protected route will be logged out before having to make a rejected api call
 
-const getCookieValue = (name) => {
-	const cookies = document.cookie.split(";");
-	for (let i = 0; i < cookies.length; i++) {
-		const cookie = cookies[i].trim();
-		if (cookie.startsWith(name + "=")) {
-			return cookie.substring(name.length + 1);
-		}
-	}
-};
 const verifyAuth = () => {
 	let authentified = true;
-	let csrfAccessToken = getCookieValue("csrf_access_token");
-
+	let csrfAccessToken = cookieService.getCookie("csrf_access_token");
 	if (!csrfAccessToken) {
+		console.log("logout");
 		authentified = false;
 	}
 	return authentified;
