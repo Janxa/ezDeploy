8c17e5944decc421edc1ada94f85cf7c70b9004f
Janxa <onizuka-54@live.fr>
2023-06-15 17:55:56 +0200

    Removing interceptor

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,26 +1,8 @@
 import axios from "axios";
-import AuthService from "./authentification.service";
 const API_URL = "/api";
 
 const api = axios.create({
 	baseURL: API_URL,
-	// Other Axios configuration options
 });
-api.interceptors.response.use(
-	(response) => response,
-	console.log("Intercepted"),
-
-	(error) => {
-		console.log("error found");
-
-		if (error.response && error.response.status === 401) {
-			// Handle the 401 response and disconnect the user
-			console.log("error code == 401");
-
-			AuthService.logout(); // Implement your logic to disconnect the user
-		}
-		return Promise.reject(error);
-	}
-);
 
 export default api;
