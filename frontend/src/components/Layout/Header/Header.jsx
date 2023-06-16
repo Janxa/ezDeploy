7e8fc2d1853cb5984c0d3c00d516a87396d463b6
Janxa <onizuka-54@live.fr>
2023-06-16 11:23:16 +0200

    Adding the menu wrapper inside the header to avoid duplicate code in menu component

--- frontend/src/components/Layout/Header/Header.jsx
+++ frontend/src/components/Layout/Header/Header.jsx
@@ -3,8 +3,10 @@ import Menu from "./Menu";
 function Header() {
 	const [visibleMobile, setVisibleMobile] = useState(false);
 	return (
-		<header className="flex bg-flat-800 justify-between py-4 px-5 h-16 md:px-16 shadow-md absolute top-0 left-0 right-0 overflow-visible">
-			<h1 className="text-chili-500 font-bold text-2xl  ">EZ Deploy</h1>
+		<header className="flex bg-flat-800 justify-between py-4 px-8 h-16 md:px-16 shadow-md absolute top-0 left-0 right-0 overflow-visible">
+			<h1 className="text-chili-500 basis-1/2 md:basis-1/3 font-bold text-2xl  ">
+				EZ Deploy
+			</h1>
 
 			<button
 				onClick={() => setVisibleMobile(!visibleMobile)}
@@ -14,7 +16,19 @@ function Header() {
 				<div className="bg-white w-full h-[13%] group-hover:w-2/3 transition-all"></div>
 				<div className="bg-white w-full h-[13%] group-hover:w-4/5 transition-all"></div>
 			</button>
-			<Menu visibleMobile={visibleMobile} setVisibleMobile={setVisibleMobile} />
+
+			<div
+				className={
+					visibleMobile
+						? "flex flex-col bg-flat-700 md:bg-flat-800 items-center absolute top-16 left-0 w-full h-screen z-50 md:flex-row md:flex md:w-3/4   md:justify-end md:font-medium md:static md:h-full pt-4 md:pt-0"
+						: "hidden md:flex md:w-2/3 md:justify-end md:font-medium "
+				}
+			>
+				<Menu
+					visibleMobile={visibleMobile}
+					setVisibleMobile={setVisibleMobile}
+				/>
+			</div>
 		</header>
 	);
 }
