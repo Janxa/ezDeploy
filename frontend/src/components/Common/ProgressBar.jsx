3a47d6e75dcc87105aa552f4369681ffe746c382
Janxa <onizuka-54@live.fr>
2023-08-04 09:58:15 +0200

    Progress bar now slowly fading to green as it progresses

--- frontend/src/components/Common/ProgressBar.jsx
+++ frontend/src/components/Common/ProgressBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import { useRef } from "react";
 const ProgressBar = ({ value, max }) => {
-	const progress = Math.round((value / max) * 100);
+	const barRef = useRef();
 
 	return (
-		<div className="h-4 w-full rounded-full bg-flat-300 relative">
+		<div className="h-4 w-full rounded-full bg-flat-800 relative ">
 			<div
-				className="h-full rounded-full bg-chili-500 "
-				style={{ width: `${progress}%`, transition: "width 0.5s ease-in-out" }}
+				className="h-full rounded-full  "
+				style={{
+					width: `${progress}%`,
+					transition: "width 0.5s ease-in-out",
+					backgroundColor: `hsl(${progress * 1.59},${
+						progress * 0.28 + 25
+					}%,54%)`,
+				}}
 			/>
 			<span className="text-xs font-bold text-chili-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 				{`${progress}%`}
