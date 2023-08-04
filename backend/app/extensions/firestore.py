b6ac5a9fdcfb136423e67716417d2170172d5758
Janxa <onizuka-54@live.fr>
2023-08-04 18:29:08 +0200

    Reworking get_document and find_single_document

--- backend/app/extensions/firestore.py
+++ backend/app/extensions/firestore.py
@@ -1,4 +1,5 @@
 from firebase_admin import credentials, firestore, initialize_app
+from google.cloud.firestore_v1.base_query import FieldFilter
 from flask import Flask,has_app_context,current_app
 
 class FlaskFirestore:
@@ -12,10 +13,13 @@ class FlaskFirestore:
 
         message = f"{type(self).__name__} {self.firebase_app} {self.firestore_db}"
         return f"<{message}>"
+    @staticmethod
+    def delete_field():
+        return firestore.DELETE_FIELD
 
     def init_firestore(self,app) -> firestore.client:
         if "firestore" in app.extensions:
-            raise RuntimeError( "A 'Firestpre' instance has already been registered on this Flask app.")
+            raise RuntimeError( "A 'Firestore' instance has already been registered on this Flask app.")
 
         creds=credentials.Certificate(app.config["FIRESTORE_KEY"])
         self.firebase_app = initialize_app(creds)
@@ -24,22 +28,25 @@ class FlaskFirestore:
 
         return self.firestore_client
 
-    def get_document(self, collection, document_id):
+    def get_document(self, collection, document_id, ref=False):
             doc_ref = self.firestore_client.collection(collection).document(document_id)
             doc = doc_ref.get()
-            if doc.exists:
+            if doc.exists and not ref:
                 return doc.to_dict()
+            elif doc.exists and ref:
+                return doc_ref
             return None
+
     def find_single_document(self, collection, key,value):
-            docs = self.firestore_client.collection(collection).where(str(key),"==",str(value)).limit(1).stream()
+            docs = self.firestore_client.collection(collection).where(filter=FieldFilter(str(key),"==",str(value))).limit(1).stream()
             try:
                 doc = next(docs)
-                return {doc.id: doc.to_dict()}
+                return {"id":doc.id,"doc": doc.to_dict()}
             except StopIteration:
                 return None
-    def add_document(self,  collection, data):
+    def add_document(self,  collection, data) :
             doc_ref = self.firestore_client.collection(collection).document()
-            doc_ref.update(data)
+            doc_ref.set(data)
             return doc_ref.id
 
     def update_document(self,  collection, document_id, data):
