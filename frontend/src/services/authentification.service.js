6c174ba4f8c3cceb5612abcf67d18e35216a14d4
Janxa <onizuka-54@live.fr>
2023-09-15 19:36:33 +0200

    Adding Password reset and email token resend service

--- frontend/src/services/authentification.service.js
+++ frontend/src/services/authentification.service.js
@@ -11,18 +11,38 @@ const AuthService = {
 		localStorage.setItem("user", JSON.stringify(res.data.username));
 		return res;
 	},
+
 	logout() {
 		document.cookie =
 			"csrf_access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 		localStorage.removeItem("user");
 	},
-	register(username, email, password) {
-		return axios.post(AUTH_URL + "/register", {
+
+	async register(username, email, password) {
+		return api.post(AUTH_URL + "/register", {
 			username: username,
 			email: email,
 			password: password,
 		});
 	},
+
+	async sendNewEmailToken({ email = false, user_id = false }) {
+		if (user_id) {
+			return await api.post(AUTH_URL + "/resend", {
+				user_id: user_id,
+			});
+		} else if (email) {
+			return await api.post(AUTH_URL + "/resend", {
+				email: email,
+			});
+		}
+		return null;
+	},
+
+	async sendResetPasswordEmail(email) {
+		return await api.post(AUTH_URL + "/password/resend", { email: email });
+	},
+
 	getCurrentUser() {
 		try {
 			const user = JSON.parse(localStorage.getItem("user"));
