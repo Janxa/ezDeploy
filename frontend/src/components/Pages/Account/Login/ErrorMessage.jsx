0936a1f9642d0ceb84ef73dc46d5782c034931e9
Janxa <onizuka-54@live.fr>
2023-09-15 19:51:51 +0200

    Reworking error message for User not validated usecase

--- frontend/src/components/Pages/Account/Login/ErrorMessage.jsx
+++ frontend/src/components/Pages/Account/Login/ErrorMessage.jsx
@@ -1,16 +1,31 @@
-function ErrorMessage({ message, onButtonClick,buttonContent}) {
-    return (
-      <div className="font-medium text-sm py-1 text-color-red-primary">
-        {message}
-        {onButtonClick && (
-          <button
-            onClick={onButtonClick}
-            className="font-medium text-color-blue-primary underline decoration-dashed"
-          >
-            {buttonContent}
-          </button>
-        )}
-      </div>
-    );
-  }
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+
+function ErrorMessage({ message, onButtonClick }) {
+	const renderButton = message === "User not validated";
+
+	if (renderButton) {
+		message = "Your email has not been validated, please check your emails or ";
+	}
+
+	return (
+		<div className="font-medium text-sm py-1 text-color-red-primary">
+			<p className="text-md my-2 text-center w-fulls text-invalid-500 self-center">
+				<FontAwesomeIcon icon={faTriangleExclamation} />
+				{message}
+
+				{renderButton && (
+					<span
+						onClick={onButtonClick}
+						className="font-medium  underline decoration-dashed inline cursor-pointer"
+					>
+						Resend One
+					</span>
+				)}
+			</p>
+		</div>
+	);
+}
+
 export default ErrorMessage;
