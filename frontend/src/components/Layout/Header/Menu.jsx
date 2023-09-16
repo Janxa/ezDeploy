b9041eac3c2873e056ac423e1f13545625edf5b8
Janxa <onizuka-54@live.fr>
2023-09-16 09:44:35 +0200

    Improving menu css

--- frontend/src/components/Layout/Header/Menu.jsx
+++ frontend/src/components/Layout/Header/Menu.jsx
@@ -22,11 +22,11 @@ function Menu({ visibleMobile, setVisibleMobile }) {
 					<Link to="contact">Contact</Link>
 				</li>
 				<li className="flex font-bold mb-4 md:mb-0  items-center justify-around gap-x-4 lg:gap-x-8 self-center underline ">
-					<div className="text-lila-200 hover:text-lila-300 transition-colors ease-in-out duration-100">
+					<div className="text-lila-200 flex items-center hover:text-lila-300 transition-colors ease-in-out duration-100">
 						<Link to="dashboard">Dashboard</Link>
 						<FontAwesomeIcon icon={faTable} className="fa-xs ml-2 " />
 					</div>
-					<div className="text-lila-200 hover:text-lila-300 transition-colors ease-in-out duration-100">
+					<div className="text-lila-200 flex items-center hover:text-lila-300 transition-colors ease-in-out duration-100">
 						<Link to="uploader">Upload </Link>
 						<FontAwesomeIcon
 							icon={faArrowUpFromBracket}
