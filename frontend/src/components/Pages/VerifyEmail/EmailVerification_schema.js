b73511113b3ce2bb4c0847915f4e1c9ec4f52265
Janxa <onizuka-54@live.fr>
2023-09-16 09:37:48 +0200

    Removing hex constraint

--- frontend/src/components/Pages/VerifyEmail/EmailVerification_schema.js
+++ frontend/src/components/Pages/VerifyEmail/EmailVerification_schema.js
@@ -1,8 +1,7 @@
 import Joi from "joi";
 
 const EmailVerificationSchema = Joi.object({
-	code: Joi.string().hex().length(32).required().messages({
-		"string.hex": "Token is invalid",
+	code: Joi.string().length(32).required().messages({
 		"string.required": "Token is required",
 		"any.invalid": "Token is invalid",
 	}),
