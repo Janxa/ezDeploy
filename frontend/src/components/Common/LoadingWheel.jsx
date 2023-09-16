34f3b6cc674255aac93441e5efe214178396d0be
Janxa <onizuka-54@live.fr>
2023-09-16 09:45:56 +0200

    LoadingWheel now accepts custom styles

--- frontend/src/components/Common/LoadingWheel.jsx
+++ frontend/src/components/Common/LoadingWheel.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const LoadingWheel = ({ color = "white" }) => {
+const LoadingWheel = ({ color = "white", extraStyle }) => {
 	return (
 		<div className="flex justify-center items-center">
 			<div
-				className={`animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-${color}`}
+				className={`animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-${color} ${extraStyle}`}
 			></div>
 		</div>
 	);
